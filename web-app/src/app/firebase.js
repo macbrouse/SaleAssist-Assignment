f1455e10e97175bec24f5a36dc281d531952c06e
Sarthak Varmani <sarthakvarmani@MacBook-Pro.local>
2025-02-28 23:01:06 +0530

    dotenv fix

--- web-app/src/app/firebase.js
+++ web-app/src/app/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth,GoogleAuthProvider } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
+require('dotenv').config();
 
 const firebaseKey=process.env.FIREBASE
 
